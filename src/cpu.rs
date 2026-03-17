013b8e0098292bae7eabb741d40d50df3753260b
Astgenne <daniele.candido@tutamail.com>
2026-03-17 17:12:59 +0100

    chore(cpu): calmed clippy

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,11 +21,11 @@ const FONT: [u8; 80] = [
 
 pub struct OPCODE {
     code: u8,
-    X: u8,
-    Y: u8,
-    N: u8,
-    NN: u8,
-    NNN: u16,
+    x: u8,
+    y: u8,
+    n: u8,
+    nn: u8,
+    nnn: u16,
 }
 
 #[derive(Clone)]
@@ -55,25 +55,25 @@ impl PartialEq<u8> for Key {
     }
 }
 
-impl Into<u8> for Key {
-    fn into(self) -> u8 {
-        match self {
-            Self::ZERO => 0x0,
-            Self::ONE => 0x1,
-            Self::TWO => 0x2,
-            Self::THREE => 0x3,
-            Self::FOUR => 0x4,
-            Self::FIVE => 0x5,
-            Self::SIX => 0x6,
-            Self::SEVEN => 0x7,
-            Self::EIGHT => 0x8,
-            Self::NINE => 0x9,
-            Self::A => 0xA,
-            Self::B => 0xB,
-            Self::C => 0xC,
-            Self::D => 0xD,
-            Self::E => 0xE,
-            Self::F => 0xF,
+impl From<Key> for u8 {
+    fn from(val: Key) -> Self {
+        match val {
+            Key::ZERO => 0x0,
+            Key::ONE => 0x1,
+            Key::TWO => 0x2,
+            Key::THREE => 0x3,
+            Key::FOUR => 0x4,
+            Key::FIVE => 0x5,
+            Key::SIX => 0x6,
+            Key::SEVEN => 0x7,
+            Key::EIGHT => 0x8,
+            Key::NINE => 0x9,
+            Key::A => 0xA,
+            Key::B => 0xB,
+            Key::C => 0xC,
+            Key::D => 0xD,
+            Key::E => 0xE,
+            Key::F => 0xF,
         }
     }
 }
@@ -82,7 +82,7 @@ pub struct CPU {
     memory: [u8; 4096],
     registers: [u8; 16],
     pc: usize,
-    I: u16,
+    i: u16,
     stack: Vec<u16>,
     display: [[bool; 64]; 32],
     delay_timer: u8,
@@ -98,7 +98,7 @@ impl Default for CPU {
             memory,
             registers: [0; 16],
             pc: 0x200,
-            I: 0,
+            i: 0,
             stack: Vec::with_capacity(16),
             display: [[false; 64]; 32],
             delay_timer: 0,
@@ -146,17 +146,17 @@ impl CPU {
     fn decode(&mut self, instruction: u16) -> OPCODE {
         OPCODE {
             code: ((instruction & 0xF000) >> 12) as u8,
-            X: ((instruction & 0x0F00) >> 8) as u8,
-            Y: ((instruction & 0x00F0) >> 4) as u8,
-            N: (instruction & 0x000F) as u8,
-            NN: (instruction & 0x00FF) as u8,
-            NNN: instruction & 0x0FFF,
+            x: ((instruction & 0x0F00) >> 8) as u8,
+            y: ((instruction & 0x00F0) >> 4) as u8,
+            n: (instruction & 0x000F) as u8,
+            nn: (instruction & 0x00FF) as u8,
+            nnn: instruction & 0x0FFF,
         }
     }
 
     fn execute(&mut self, instruction: OPCODE, pressed_key: Option<Key>) {
         match instruction.code {
-            0x00 => match instruction.NNN {
+            0x00 => match instruction.nnn {
                 // 00E0 -> Clear Screen
                 0x0E0 => self.display = [[false; 64]; 32],
                 // 00EE -> Return from subroutine
@@ -166,15 +166,15 @@ impl CPU {
                 _ => unreachable!(),
             },
             // 1NNN -> Jump
-            0x01 => self.pc = instruction.NNN as usize,
+            0x01 => self.pc = instruction.nnn as usize,
             // 2NNN -> Call subroutine
             0x02 => {
                 self.stack.push(self.pc as u16);
-                self.pc = instruction.NNN as usize;
+                self.pc = instruction.nnn as usize;
             }
             // 3XNN -> Skip one if VX == NN
             0x03 => {
-                self.pc += if self.registers[instruction.X as usize] == instruction.NN {
+                self.pc += if self.registers[instruction.x as usize] == instruction.nn {
                     2
                 } else {
                     0
@@ -182,7 +182,7 @@ impl CPU {
             }
             // 4XNN -> Skip one if VX != NN
             0x04 => {
-                self.pc += if self.registers[instruction.X as usize] != instruction.NN {
+                self.pc += if self.registers[instruction.x as usize] != instruction.nn {
                     2
                 } else {
                     0
@@ -190,91 +190,91 @@ impl CPU {
             }
             // 5XY0 -> Skip one if VX == VY
             0x05 => {
-                self.pc += if self.registers[instruction.X as usize] == self.registers[instruction.Y as usize] {
+                self.pc += if self.registers[instruction.x as usize] == self.registers[instruction.y as usize] {
                     2
                 } else {
                     0
                 };
             }
             // 6XNN -> Set VX to NN
-            0x06 => self.registers[instruction.X as usize] = instruction.NN,
+            0x06 => self.registers[instruction.x as usize] = instruction.nn,
             // 7XNN -> Add NN to VX
             0x07 => {
-                self.registers[instruction.X as usize] =
-                    self.registers[instruction.X as usize].overflowing_add(instruction.NN).0;
+                self.registers[instruction.x as usize] =
+                    self.registers[instruction.x as usize].overflowing_add(instruction.nn).0;
             }
-            0x08 => match instruction.N {
+            0x08 => match instruction.n {
                 // 8XY0 -> Set VX to VY
-                0x00 => self.registers[instruction.X as usize] = self.registers[instruction.Y as usize],
+                0x00 => self.registers[instruction.x as usize] = self.registers[instruction.y as usize],
                 // 8XY1 -> Set VX to VX OR VY
-                0x01 => self.registers[instruction.X as usize] |= self.registers[instruction.Y as usize],
+                0x01 => self.registers[instruction.x as usize] |= self.registers[instruction.y as usize],
                 // 8XY2 -> Set VX to VX AND VY
-                0x02 => self.registers[instruction.X as usize] &= self.registers[instruction.Y as usize],
+                0x02 => self.registers[instruction.x as usize] &= self.registers[instruction.y as usize],
                 // 8XY3 -> Set VX to VX XOR VY
-                0x03 => self.registers[instruction.X as usize] ^= self.registers[instruction.Y as usize],
+                0x03 => self.registers[instruction.x as usize] ^= self.registers[instruction.y as usize],
                 // 8XY4 -> Add VY to VX (with carry flag)
                 0x04 => {
                     let (sum, carry) =
-                        self.registers[instruction.X as usize].overflowing_add(self.registers[instruction.Y as usize]);
-                    self.registers[instruction.X as usize] = sum;
+                        self.registers[instruction.x as usize].overflowing_add(self.registers[instruction.y as usize]);
+                    self.registers[instruction.x as usize] = sum;
                     self.registers[0xF] = if carry { 1 } else { 0 };
                 }
                 // 8XY5 -> Subtract VY from VX (with carry flag)
                 0x05 => {
                     let (sub, carry) =
-                        self.registers[instruction.X as usize].overflowing_sub(self.registers[instruction.Y as usize]);
-                    self.registers[instruction.X as usize] = sub;
+                        self.registers[instruction.x as usize].overflowing_sub(self.registers[instruction.y as usize]);
+                    self.registers[instruction.x as usize] = sub;
                     self.registers[0xF] = if carry { 0 } else { 1 };
                 }
                 // 8XY6 -> Set VX to VX << 1 (with carry flag)
                 0x06 => {
-                    self.registers[0xF] = (self.registers[instruction.X as usize] & 0x80) >> 7;
-                    self.registers[instruction.X as usize] <<= 1;
+                    self.registers[0xF] = (self.registers[instruction.x as usize] & 0x80) >> 7;
+                    self.registers[instruction.x as usize] <<= 1;
                 }
                 // 8XY7 -> Subtract VX from VY (with carry flag)
                 0x07 => {
                     let (sub, carry) =
-                        self.registers[instruction.Y as usize].overflowing_sub(self.registers[instruction.X as usize]);
-                    self.registers[instruction.Y as usize] = sub;
+                        self.registers[instruction.y as usize].overflowing_sub(self.registers[instruction.x as usize]);
+                    self.registers[instruction.y as usize] = sub;
                     self.registers[0xF] = if carry { 0 } else { 1 };
                 }
                 // 8XYE -> Set VX to VX >> 1 (with carry flag)
                 0x0E => {
-                    self.registers[0xF] = self.registers[instruction.X as usize] & 0x1;
-                    self.registers[instruction.X as usize] >>= 1;
+                    self.registers[0xF] = self.registers[instruction.x as usize] & 0x1;
+                    self.registers[instruction.x as usize] >>= 1;
                 }
                 _ => unreachable!(),
             },
             // 9XY0 -> Skip one if VX != VY
             0x09 => {
-                self.pc += if self.registers[instruction.X as usize] != self.registers[instruction.Y as usize] {
+                self.pc += if self.registers[instruction.x as usize] != self.registers[instruction.y as usize] {
                     2
                 } else {
                     0
                 };
             }
             // ANNN -> Set I to NNN
-            0x0A => self.I = instruction.NNN,
+            0x0A => self.i = instruction.nnn,
             // BNNN -> Jump to address NNN + V0
-            0x0B => self.pc = (instruction.NNN + (self.registers[0x0] as u16)) as usize,
+            0x0B => self.pc = (instruction.nnn + (self.registers[0x0] as u16)) as usize,
             // CXNN -> Sets VX to RAND & NN
-            0x0C => self.registers[instruction.X as usize] = rand::random_range(0..255) & instruction.NN,
+            0x0C => self.registers[instruction.x as usize] = rand::random_range(0..255) & instruction.nn,
             // DXYN -> Draws the display
             0x0D => {
-                let mut x = self.registers[instruction.X as usize] % 64;
-                let mut y = self.registers[instruction.Y as usize] % 32;
+                let mut y = self.registers[instruction.y as usize] % 32;
 
-                for i in 0..instruction.N as u16 {
+                for i in 0..instruction.n as u16 {
+                    let mut x = self.registers[instruction.x as usize] % 64;
                     if y >= 32 {
                         break;
                     }
-                    let s = self.memory[(self.I + i) as usize];
+                    let s = self.memory[(self.i + i) as usize];
                     for b in 0..8 {
                         if x >= 64 {
                             break;
                         }
-                        self.display[x as usize][y as usize] = ((s & (1 << (8 - b))) >> (8 - b)) == 1;
-                        if self.display[x as usize][y as usize] {
+                        self.display[y as usize][x as usize] = ((s & (1 << (7 - b))) >> (7 - b)) == 1;
+                        if self.display[y as usize][x as usize] {
                             self.registers[0xF] = 1;
                         }
                         x += 1;
@@ -282,11 +282,11 @@ impl CPU {
                     y += 1;
                 }
             }
-            0x0E => match instruction.NN {
+            0x0E => match instruction.nn {
                 // EX9E -> Skip one if key VX is pressed
                 0x9E => {
                     if let Some(key) = pressed_key {
-                        self.pc += if key == self.registers[instruction.X as usize] {
+                        self.pc += if key == self.registers[instruction.x as usize] {
                             2
                         } else {
                             0
@@ -295,7 +295,7 @@ impl CPU {
                 }
                 // EXA1 -> Skip one if key VX is not pressed
                 0xA1 => {
-                    self.pc += if pressed_key.is_none_or(|k| k != self.registers[instruction.X as usize]) {
+                    self.pc += if pressed_key.is_none_or(|k| k != self.registers[instruction.x as usize]) {
                         2
                     } else {
                         0
@@ -303,45 +303,45 @@ impl CPU {
                 }
                 _ => unreachable!(),
             },
-            0x0F => match instruction.NN {
+            0x0F => match instruction.nn {
                 // FX07 -> Sets VX to delay timer
-                0x07 => self.registers[instruction.X as usize] = self.delay_timer,
+                0x07 => self.registers[instruction.x as usize] = self.delay_timer,
                 // FX07 -> Sets delay timer to VX
-                0x15 => self.delay_timer = self.registers[instruction.X as usize],
+                0x15 => self.delay_timer = self.registers[instruction.x as usize],
                 // FX07 -> Sets sound timer to VX
-                0x18 => self.sound_timer = self.registers[instruction.X as usize],
+                0x18 => self.sound_timer = self.registers[instruction.x as usize],
                 // FX1E -> Add VX to I (with carry flag if "overflow" over 0x1000)
                 0x1E => {
-                    let (sum, carry) = self.I.overflowing_add(self.registers[instruction.X as usize] as u16);
-                    self.I = sum;
+                    let (sum, carry) = self.i.overflowing_add(self.registers[instruction.x as usize] as u16);
+                    self.i = sum;
                     self.registers[0xF] = if carry || sum >= 0x1000 { 1 } else { 0 };
                 }
                 // FX0A -> Block for key press
                 0x0A => {
                     if let Some(key) = pressed_key {
-                        self.registers[instruction.X as usize] = key.into();
+                        self.registers[instruction.x as usize] = key.into();
                     } else {
                         self.pc -= 2;
                     }
                 }
                 // FX29 -> Sets I to font character for the value in VX
-                0x29 => self.I = (self.registers[instruction.X as usize] * 5) as u16,
+                0x29 => self.i = (self.registers[instruction.x as usize] * 5) as u16,
                 // FX33 -> Sets address I, I + 1 and I + 2 to the 3 decimal digits of VX
                 0x33 => {
-                    self.memory[self.I as usize] = self.registers[instruction.X as usize] / 100;
-                    self.memory[self.I as usize + 1] = (self.registers[instruction.X as usize] % 100) / 10;
-                    self.memory[self.I as usize + 2] = self.registers[instruction.X as usize] % 10;
+                    self.memory[self.i as usize] = self.registers[instruction.x as usize] / 100;
+                    self.memory[self.i as usize + 1] = (self.registers[instruction.x as usize] % 100) / 10;
+                    self.memory[self.i as usize + 2] = self.registers[instruction.x as usize] % 10;
                 }
                 // FX55 -> Store registers 0 to X in memory
                 0x55 => {
-                    for x in 0..=instruction.X as u16 {
-                        self.memory[(self.I + x) as usize] = self.registers[x as usize];
+                    for x in 0..=instruction.x as u16 {
+                        self.memory[(self.i + x) as usize] = self.registers[x as usize];
                     }
                 }
                 // FX66 -> Load registers 0 to X from memory
                 0x65 => {
-                    for x in 0..=instruction.X as u16 {
-                        self.registers[x as usize] = self.memory[(self.I + x) as usize];
+                    for x in 0..=instruction.x as u16 {
+                        self.registers[x as usize] = self.memory[(self.i + x) as usize];
                     }
                 }
                 _ => unreachable!(),
